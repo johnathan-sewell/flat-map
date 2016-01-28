cca7f5fcf1b1158892e8d67d1acf168cd0ea04a9
Jonathan Sewell <jonathans@brandwatch.com>
2016-01-28 21:37:21 +0000

    add express server and index page with semantic-ui css

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const appConfig = {
+    appPort: 3000
+};
+
+const logger = {
+    /*eslint-disable no-console*/
+    info: console.log
+    /*eslint-enable no-console*/
+};
+
+const app = express();
+const server = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.use(express.static('public'));
+
+app.use(function(req, res) {
+    res.status(404).send('Oops, that page doesn\'t exist');
+});
+
+server.listen(appConfig.appPort, () => {
+    logger.info(`server listening on port ${appConfig.appPort}`);
+});
